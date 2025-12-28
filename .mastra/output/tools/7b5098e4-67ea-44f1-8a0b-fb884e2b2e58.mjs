915d26b2c571eff2db725a0e0818d27ffc98e09b
cotein <diego.barrueta@gmail.com>
2025-12-28 17:55:16 -0300

    first commit

--- .mastra/output/tools/7b5098e4-67ea-44f1-8a0b-fb884e2b2e58.mjs
+++ .mastra/output/tools/7b5098e4-67ea-44f1-8a0b-fb884e2b2e58.mjs
@@ -2,5 +2,5 @@ export { p as procesarNuevaPropiedad } from '../procesa-nueva-propiedad.mjs';
 import '@mastra/core/tools';
 import 'zod';
 import '@mastra/core/workflows';
-import './eac9f7b9-e8a5-4004-b1fe-7768fe6b9fb2.mjs';
+import './af1e3007-0631-4643-818c-793180fe7a59.mjs';
 import 'axios';
