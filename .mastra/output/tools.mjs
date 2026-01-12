6f9c586727d2d831b1ba93545a4983da5f209acb
cotein <diego.barrueta@gmail.com>
2026-01-12 18:38:25 -0300

    funcionando

--- .mastra/output/tools.mjs
+++ .mastra/output/tools.mjs
@@ -1,10 +1,10 @@
-import * as tool0 from './tools/ccfaa10c-8692-4519-90f2-d83a47062601.mjs';
-import * as tool1 from './tools/7daa9321-7c1b-4c51-bb9b-e416f3180640.mjs';
-import * as tool2 from './tools/490e7c3c-7ec4-4c27-8c47-2a54f4f1ce1b.mjs';
-import * as tool3 from './tools/937d1ddb-f6e7-46f2-bc37-cd9ddd4d91fe.mjs';
-import * as tool4 from './tools/096bb50e-f88d-4d40-9848-895f10cd2668.mjs';
-import * as tool5 from './tools/093f3af4-28ba-4d34-8c10-bc08b1fbe04f.mjs';
-import * as tool6 from './tools/d1c711ed-10e4-4340-9b03-eccaf9c2563f.mjs';
-import * as tool7 from './tools/866236a8-5743-4a17-8f3c-fce6b7631f38.mjs';
+import * as tool0 from './tools/1109d1c9-6cdc-4e15-84c4-fe937a5b93ca.mjs';
+import * as tool1 from './tools/3224a0ce-d44b-4616-b58e-bbedcc336077.mjs';
+import * as tool2 from './tools/62727591-b5fb-40c3-afe7-8110ea13626b.mjs';
+import * as tool3 from './tools/9f8b7982-747e-443b-9514-b95ce10d5a7b.mjs';
+import * as tool4 from './tools/196f5ee9-548b-4b2b-982d-71e2e871901e.mjs';
+import * as tool5 from './tools/3d32f117-aa71-4c19-b9dd-6555672336c7.mjs';
+import * as tool6 from './tools/a1a761b2-62a2-4485-8277-e49074c5458c.mjs';
+import * as tool7 from './tools/413b4bc0-abf9-4477-8ff5-044bb05ff5b6.mjs';
 
                 export const tools = [tool0, tool1, tool2, tool3, tool4, tool5, tool6, tool7]
